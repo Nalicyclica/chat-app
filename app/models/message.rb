5a44520d8543fcf42261360d6e9b0925ae156d87
Nalicyclica <siruri0xf3@gmail.com>
2021-07-16 14:56:52 +0900

    Revert "画像表示の実装"
    This reverts commit 8875d4d48e875f35ad06d38e7a1830f260e663a5.

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,11 +1,7 @@
 class Message < ApplicationRecord
-  validates :content, presence: true, unless: :was_attached?
+  validates :content, presence: true
 
   belongs_to :user
   belongs_to :room
   has_one_attached :image
-
-  def was_attached?
-    self.image.attached?
-  end
 end
